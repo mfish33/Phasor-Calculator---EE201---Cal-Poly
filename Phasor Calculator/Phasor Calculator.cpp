c8b95a295e55c74f386a43f3bbaed93a4e16fba6
mfish33 <32677537+mfish33@users.noreply.github.com>
2020-06-20 20:48:57 -0400

    small fixes and added example image

--- Phasor Calculator/Phasor Calculator.cpp
+++ Phasor Calculator/Phasor Calculator.cpp
@@ -1,7 +1,7 @@
 ﻿// Modules
 #include "shared.h"
 #include "Waveforms.h"
-#include "inductances.h"
+#include "impedance.h"
 #include "power.h"
 #include "filters.h"
 #include "printing.h"
@@ -9,27 +9,28 @@
 
 int main()
 {	
-	/* Example Problem 1 */
+	/* Example Problem */
 	auto Vs = sin(10, 0);
 	auto C1 = capacitor(10e-6, 2000 * pi);
 	auto L1 = inductor(10e-3, 2000 * pi);
 	auto R1 = resistor(1000);
 
 	auto Zeq = C1 + inverse(inverse(L1) + inverse(R1));
+	print("Zeq = ");
 	print(Zeq);
 	// OUTPUT Zeq = 3.93231 + 46.6692i
 
 	auto Is = Vs / Zeq;
+	print("Is = ");
 	printPhasordegfunc(Is, 2000 * pi);
 	// OUTPUT: Is = 0.21cos(2000πt-175.184)
 	auto VR1 = Vs - Is * C1;
 	auto IR1 = VR1 / R1;
+	print("IR1 = ");
 	printPhasordegfunc(IR1, 2000 * pi);
 	// OUTPUT: IR1 = 0.013cos(2000πt-88.78)
 
 
-
-	/* Example Problem 2 */
 	auto P = calcPower(Vs, Is);
 	printPower(P);
 	//OUTPUT: Real Power: 0.0896364 imaginary power: 1.06382 reactive power: 1.06759
